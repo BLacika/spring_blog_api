086aa70a06b491098d9ba4a93a238276280d5fd3
BLacika <b.lacika23@gmail.com>
2020-07-14 17:13:32 +0200

    Added Post Model

---
+++ src/main/java/com/bala/spring_blog_api/model/Post.java
@@ -0,0 +1,44 @@
+package com.bala.spring_blog_api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String author;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Lob
+    private String content;
+
+    private String status;
+
+    private String tags;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}

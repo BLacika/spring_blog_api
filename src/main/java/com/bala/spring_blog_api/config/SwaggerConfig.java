54ad3e9ab81a1f10edaff2ebb16134a3222e83b4
BLacika <b.lacika23@gmail.com>
2020-07-14 12:42:48 +0200

    Update SwaggerConfig.java

--- src/main/java/com/bala/spring_blog_api/config/SwaggerConfig.java
+++ src/main/java/com/bala/spring_blog_api/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.bala.spring_blog_api.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }

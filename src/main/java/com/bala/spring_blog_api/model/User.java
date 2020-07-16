41343e913bc200e79bedc969287ed49949bae437
BLacika <b.lacika23@gmail.com>
2020-07-16 11:22:05 +0200

    Added User Model

---
+++ src/main/java/com/bala/spring_blog_api/model/User.java
@@ -0,0 +1,47 @@
+package com.bala.spring_blog_api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank
+    private String username;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String roles;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
+}
